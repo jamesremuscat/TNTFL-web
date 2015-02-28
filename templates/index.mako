35c062c1f25e3886adcaba2d76c04d506964ffcd
James Muscat <jamesremuscat@gmail.com>
2015-02-28 00:58:21 +0000

    Calculate player skill.

--- templates/index.mako
+++ templates/index.mako
@@ -1,2 +1,3 @@
 <%inherit file="html.mako" />
-Ladder has ${len(ladder.games)} games
+Ladder has ${len(ladder.games)} games<br />
+${sorted([p for p in ladder.players.values()], key=lambda x: x.elo, reverse=True)}<br />
--- tntfl/ladder.py
+++ tntfl/ladder.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
+
 class TableFootballLadder(object):
 
     games = []
+    players = {}
 
     def __init__(self, ladderFile):
         ladder = open(ladderFile, 'r')
@@ -12,13 +16,51 @@ class TableFootballLadder(object):
                 self.addGame(game)
 
     def addGame(self, game):
+        if game.redPlayer not in self.players:
+            self.players[game.redPlayer] = Player(game.redPlayer)
+        red = self.players[game.redPlayer]
+
+        if game.bluePlayer not in self.players:
+            self.players[game.bluePlayer] = Player(game.bluePlayer)
+        blue = self.players[game.bluePlayer]
+
+        predict = 1 / (1 + 10 ** ((red.elo - blue.elo) / 180))
+        result = float(game.blueScore) / (game.blueScore + game.redScore)
+        delta = 25 * (result - predict)
+
+        blue.game(delta, game.time)
+        red.game(-delta, game.time)
         self.games.append(game)
 
 
 class Game(object):
     def __init__(self, redPlayer, redScore, bluePlayer, blueScore, time):
-        self.redPlayer = redPlayer
+        self.redPlayer = redPlayer.lower()
         self.redScore = int(redScore)
-        self.bluePlayer = bluePlayer
+        self.bluePlayer = bluePlayer.lower()
         self.blueScore = int(blueScore)
         self.time = time
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return "{redPlayer} {redScore}-{blueScore} {bluePlayer}".format(redPlayer=self.redPlayer, bluePlayer=self.bluePlayer, redScore=self.redScore, blueScore=self.blueScore)
+
+
+class Player(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.elo = 0.0
+        self.games = 0
+
+    def game(self, delta, time):
+        self.elo += delta
+        self.games += 1
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return self.name + ":" + str(self.elo)
