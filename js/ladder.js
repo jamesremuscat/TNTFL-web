jamesremuscat/TNTFL-web

commit 292297996e8ddf2a3ba93962ffde4006b1cc8936

Hack it till ladder sorting works again.

js/ladder.js
@@ -106,6 +106,15 @@ function getSortOptions(tableQuery) {
   return hdrorder;
 }
 
+function postLadder(sortOpts, showInactive) {
+  $("#ladder").tablesorter({'sortList': [[sortOpts[0], sortOpts[1]]], 'headers': { 11: { 'sorter': false}}});
+  $("#ladder").floatThead();
+  if (showInactive) {
+    $('.inactive').show();
+    $('.button_active').hide();
+  }
+}
+
 function reloadLadder(dates) {
   var sortOpts = getSortOptions("#ladder th");
   var showInactive = 0;
@@ -118,16 +127,7 @@ function reloadLadder(dates) {
   $("#ladderHolder").empty();
   var spinner = new Spinner().spin();
   $("#ladderHolder").append(spinner.el);
-  $("#ladderHolder").load("ladder.cgi" + dates,
-    function() {
-      $("#ladder").tablesorter({'sortList': [[sortOpts[0], sortOpts[1]]], 'headers': { 11: { 'sorter': false}}});
-      $("#ladder").floatThead();
-      if (showInactive) {
-        $('.inactive').show();
-        $('.button_active').hide();
-      }
-    }
-  );
+  $("#ladderHolder").load("ladder.cgi" + dates, postLadder(sortOpts, showInactive));
 }
 
 function updateLadderTo(range) {

templates/index.mako
@@ -33,6 +33,14 @@
           ${self.blocks.render("recent", base=self.attr.base, games=ladder.games)}
         </div>
         <script type="text/javascript">
+          var sortOpts = getSortOptions("#ladder th");
+          var showInactive = 0;
+          $("#showInactiveButtons").each(function(index) {
+            if ($(this).hasClass('inactive') && $(this).style.display == "table-row") {
+              showInactive = 1;
+            }
+          });
+          postLadder(sortOpts, showInactive);
           setInterval(function() {$("#recentHolder").load("recent.cgi")}, 600000);
         </script>
       </div>

commit b3901bbbdc24c9a08356dc29031a4f068b6c2b69

Undo hacking/breaking of sorting in historic view introduced by 292297996e8ddf2a3ba93962ffde4006b1cc8936.

js/ladder.js
@@ -127,7 +127,16 @@ function reloadLadder(dates) {
   $("#ladderHolder").empty();
   var spinner = new Spinner().spin();
   $("#ladderHolder").append(spinner.el);
-  $("#ladderHolder").load("ladder.cgi" + dates, postLadder(sortOpts, showInactive));
+  $("#ladderHolder").load("ladder.cgi" + dates,
+    function() {
+      $("#ladder").tablesorter({'sortList': [[sortOpts[0], sortOpts[1]]], 'headers': { 11: { 'sorter': false}}});
+      $("#ladder").floatThead();
+      if (showInactive) {
+        $('.inactive').show();
+        $('.button_active').hide();
+      }
+    }
+  );
 }
 
 function updateLadderTo(range) {