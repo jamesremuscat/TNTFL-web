4e812bafc52c1c7013ab1a4970499a66214c070e
James Muscat <james.muscat@alces-software.com>
2016-05-12 13:08:55 +0100

    WSGIfy the game methods.

--- game.cgi
+++ game.cgi
@@ -3,29 +3,14 @@
 import cgi
 from time import time
 from tntfl.ladder import Game, TableFootballLadder
-from tntfl.web import redirect_302, serve_template
+from tntfl.web import serve
+from wsgi import game_add, game_show
 
 form = cgi.FieldStorage()
 
 if "method" in form:
-    ladder = TableFootballLadder("ladder.txt")
     if form["method"].value == "add":
-        if "bluePlayer" in form and "redPlayer" in form:
-            redScore = form["redScore"].value if "redScore" in form else 0
-            blueScore = form["blueScore"].value if "blueScore" in form else 0
-            game = Game(form["redPlayer"].value, redScore, form["bluePlayer"].value, blueScore, time())
-            ladder.addAndWriteGame(game)
-            if "view" in form and form["view"].value == "json":
-                serve_template("wrappedGame.mako", game=game)
-            else:
-                redirect_302("../%.0f" % game.time)
+        serve(game_add())
     elif form["method"].value == "view" and "game" in form:
         gameTime = int(form["game"].value)
-        found = False
-        for game in ladder.games:
-            if game.time == gameTime and not found:
-                serve_template("wrappedGame.mako", game=game)
-                found = True
-        if not found:
-            print "Status: 404 Not Found"
-            print
+        serve(game_show(gameTime))
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
-from flask import Flask
-from tntfl.ladder import TableFootballLadder
+from flask import abort, Flask, redirect, request
+from tntfl.ladder import Game, TableFootballLadder
 from tntfl.web import get_template
+
+import time
 
 
 app = Flask(__name__)
@@ -19,5 +21,32 @@ def index():
 def stats():
     return get_template("stats.mako", ladder=getLadder())
 
+
+@app.route("/game/add/", methods=['GET', 'POST'])
+def game_add():
+    ladder = getLadder()
+    form = request.form
+    if "bluePlayer" in form and "redPlayer" in form:
+            redScore = form["redScore"] if "redScore" in form else 0
+            blueScore = form["blueScore"] if "blueScore" in form else 0
+            game = Game(form["redPlayer"], redScore, form["bluePlayer"], blueScore, time.time())
+            ladder.addAndWriteGame(game)
+            if "view" in form and form["view"] == "json":
+                return get_template("wrappedGame.mako", game=game)
+            else:
+                return redirect("/game/%.0f" % game.time)
+
+
+@app.route("/game/<int:gameTime>/")
+def game_show(gameTime):
+    found = False
+    for game in getLadder().games:
+        if game.time == gameTime and not found:
+            return get_template("wrappedGame.mako", game=game)
+            found = True
+    if not found:
+        abort(404)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
