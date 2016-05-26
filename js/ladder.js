b8501dc48df9375c8d40d7fa3e6979bc1c4e3faa
James Muscat <james.muscat@alces-software.com>
2016-05-26 11:33:30 +0100

    Add some validation to the submit form:
    - all fields filled in
    - all scores numeric
    - each score 0 <= score <= 10
    
    Note this still allows jrem/esm "deviant" scores ;-)

--- js/ladder.js
+++ js/ladder.js
@@ -160,3 +160,28 @@ function initHistorySlider(id, fromTime, toTime) {
       }
   });
 }
+
+function checkGameSubmitForm() {
+  const allInputs = $(".game-entry input").map(function() { return $(this).val() !== ""}).get();
+  var allFilled = true;
+  for (var i = 0; i < allInputs.length; i++) {
+    allFilled = allFilled && allInputs[i];
+  }
+
+  const redScore = $('input.red.score').val();
+  const blueScore = $('input.blue.score').val();
+
+  const scoreValid = function(score) {
+    if (isNaN(score)) {
+      return false;
+    }
+    if (score < 0) {
+      return false;
+    }
+    if (score > 10) {
+      return false;
+    }
+    return true;
+  }
+  $('#gameFormSubmit').prop("disabled", !allFilled || !scoreValid(redScore) || !scoreValid(blueScore));
+}
